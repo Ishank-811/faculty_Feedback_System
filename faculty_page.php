98279151011178c83c02895df4cae0a67cd1c810
Anusha Singh <70128407+anushasingh00@users.noreply.github.com>
2020-09-17 02:27:20 +0530

    frontend change

--- faculty_page.php
+++ faculty_page.php
@@ -1,3 +1,4 @@
+
 <?php
 
 
@@ -18,10 +19,30 @@
     <title>FACULTY PAGE</title>
     <style>
     body{
-      background-color:darkgrey;
+     
       padding-left:50px;
       padding-right:50px;
+      background: rgb(58,52,157);
+      background: linear-gradient(90deg, rgba(58,52,157,1) 9%, rgba(209,0,255,1) 100%);
+
     }
+    .n{
+            text-align:center;
+            background-color:purple;
+           
+         
+        }
+        .n h4{
+          color:white;
+          
+        }
+        #myTable{
+          background-color:purple;
+        }
+
+          
+        }
+
   
     </style>
   </head>
@@ -32,7 +53,16 @@ if(isset($_SESSION['loggedin']) && $_SESSION['loggedin']=true){
   
     echo'
  
-    <h1>welcome '.$_SESSION['username'].'</h1>
+    <nav class="n">
+  <a class="navbar-brand" href="facultyfeed.php"><h4> WELCOME '.$_SESSION['username'].' ! <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-emoji-laughing" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path fill-rule="evenodd" d="M12.331 9.5a1 1 0 0 1 0 1A4.998 4.998 0 0 1 8 13a4.998 4.998 0 0 1-4.33-2.5A1 1 0 0 1 4.535 9h6.93a1 1 0 0 1 .866.5z"/>
+  <path d="M7 6.5c0 .828-.448 0-1 0s-1 .828-1 0S5.448 5 6 5s1 .672 1 1.5zm4 0c0 .828-.448 0-1 0s-1 .828-1 0S9.448 5 10 5s1 .672 1 1.5z"/>
+</svg></h4></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+</nav>
 ';}
 else{
  echo'
@@ -42,6 +72,11 @@ else{
 
 
 ?>
+<br>
+<br>
+<br>
+<br>
+<br>
   </tbody>
 <table>
 
